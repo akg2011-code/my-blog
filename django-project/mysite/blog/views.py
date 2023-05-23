da09e285992aa65744445e02744bb60f1adc6072
ahmed_korany <a.korany@elham.sa>
2023-05-23 10:57:31 +0300

    add dummy data for templates

--- django-project/mysite/blog/views.py
+++ django-project/mysite/blog/views.py
@@ -1,24 +1,54 @@
+from datetime import date
 from django.shortcuts import render
 
 # Create your views here.
-posts = [
+all_posts = [
     {
         'slug': 'first-post',
-        'image': 'https://picsum.photos/200/300',
+        'image': 'https://picsum.photos/200/251',
         'author': 'CoreyMS',
-        'date': 'August 27, 2018',
+        'date': date(2023, 8, 27),
         'title': 'My First Blog Post',
         'excerpt': 'This is my first blog post with more content inside',
         'content': "Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam, voluptatum. Quisquam, voluptatum. Quisquam, voluptatum.Quisquam, voluptatum. Quisquam, voluptatum. Quisquam, voluptatum.Quisquam, voluptatum. Quisquam, voluptatum. Quisquam, voluptatum"
+     },{
+        'slug': 'second-post',
+        'image': 'https://picsum.photos/200/249',
+        'author': 'CoreyMS',
+        'date': date(2023, 8, 28),
+        'title': 'My second Blog Post',
+        'excerpt': 'This is my first blog post with more content inside',
+        'content': "Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam, voluptatum. Quisquam, voluptatum. Quisquam, voluptatum.Quisquam, voluptatum. Quisquam, voluptatum. Quisquam, voluptatum.Quisquam, voluptatum. Quisquam, voluptatum. Quisquam, voluptatum"
+     },{
+        'slug': 'third-post',
+        'image': 'https://picsum.photos/200/255',
+        'author': 'CoreyMS',
+        'date': date(2023, 8, 29),
+        'title': 'My third Blog Post',
+        'excerpt': 'This is my third blog post with more content inside',
+        'content': "Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam, voluptatum. Quisquam, voluptatum. Quisquam, voluptatum.Quisquam, voluptatum. Quisquam, voluptatum. Quisquam, voluptatum.Quisquam, voluptatum. Quisquam, voluptatum. Quisquam, voluptatum"
      }
 ]
-def starting_page(request):
-    return render(request, 'blog/index.html')
+
+def get_date(post):
+    return post['date']
+
+def starting_page(request): 
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, 'blog/index.html', {
+        'posts': latest_posts
+    })
 
 def posts(request):
-    return render(request, 'blog/all-posts.html')
+    return render(request, 'blog/all-posts.html', {
+        "all_posts": all_posts,
+    })
    
 
 def post_detail(request, slug):
-    return render(request, 'blog/post-detail.html')        
+    next_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, 'blog/post-detail.html', {
+        'post': next_post
+    })        
    
